5673a25a9eaaccf725921ed9416ce31079aaa707
Glenn Velupillai <glennselwynvel@gmail.com>
2018-04-27 18:01:21 -0400

    commented code

--- geodump.py
+++ geodump.py
@@ -10,6 +10,7 @@ fhand = codecs.open('where.js','w', "utf-8")
 fhand.write("myData = [\n")
 count = 0
 for row in cur :
+    # get geocode data and decode it
     data = str(row[1].decode())
     try: js = json.loads(str(data))
     except: continue
